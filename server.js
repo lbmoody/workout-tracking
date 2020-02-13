716a2d00f1a90daa617f1b7bf5c4f86ec53170fe
lmoody <l.moody@continuumonline.com>
2020-02-12 20:10:49 -0700

    updated document formatting and added server.js and workout model

---
+++ server.js
@@ -0,0 +1,26 @@
+// npm dependancies 
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// express set-up
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// express middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// mongoDB connect
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+// routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// server start
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
