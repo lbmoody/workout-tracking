b26bbcec02040e0b3aef2e8ef412fb7fa3e2f56a
lmoody <l.moody@continuumonline.com>
2020-02-12 21:28:34 -0700

    adjusting put route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,23 @@
 const router = require("express").Router();
 const { Workout } = require("../models");
 
-router.get("/api/workouts", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
     Workout.find()
         .then(data => res.status(200).json(data))
-        .catch(err => res.status(401).json(err))
+        .catch(err => res.status(404).json(err))
 });
 
 router.post("/api/workouts", (req, res) => {
     Workout.create(req.body)
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
+        .then(data => res.status(201).json(data))
+        .catch(err => res.status(422).json(err))
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-    console.log(req.body);
-    Workout.findByIdAndUpdate(req.params.id, {$push: req.body}, { new: true })
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
+    console.log(req.body, req.params.id);
+    Workout.findByIdAndUpdate(req.params.id, { $push: {exercises: req.body} }, { new: true })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(422).json(err))
 });
 
 module.exports = router;
